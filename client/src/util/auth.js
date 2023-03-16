d79ce16735df9a6fa6885b07d04af5878e9203c9
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-03-16 17:51:37 +0800

    refactor: change function statements to arrow functions

--- client/src/util/auth.js
+++ client/src/util/auth.js
@@ -1,14 +1,14 @@
 import { redirect } from "react-router-dom";
 
-export function getTokenDuration() {
+export const getTokenDuration = () => {
     const storedExpirationDate = localStorage.getItem("expiration");
     const expirationDate = new Date(storedExpirationDate);
     const now = new Date();
     const duration = expirationDate.getTime() - now.getTime();
     return duration;
-}
+};
 
-export function getAuthToken() {
+export const getAuthToken = () => {
     const token = localStorage.getItem("token");
 
     if (!token) {
@@ -21,16 +21,16 @@ export function getAuthToken() {
     }
 
     return token;
-}
+};
 
-export function tokenLoader() {
+export const tokenLoader = () => {
     return getAuthToken();
-}
+};
 
-export function authChecker() {
+export const authChecker = () => {
     if (!getAuthToken()) {
         return redirect("/login");
     }
 
     return null;
-}
+};
