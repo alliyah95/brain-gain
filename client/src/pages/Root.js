8e6d0c38dbc4b9bc9dee36d3835a9bf2a4156eee
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-03-16 17:35:09 +0800

    feat: implement automatic logout

--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/Auth/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { getTokenDuration } from "../util/auth";
 
 const Root = () => {
+    const submit = useSubmit();
+    const token = useLoaderData();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        if (token === "EXPIRED") {
+            submit(null, { action: "/logout", method: "POST" });
+        }
+
+        const tokenDuration = getTokenDuration();
+
+        setTimeout(() => {
+            submit(null, { action: "/logout", method: "post" });
+        }, tokenDuration);
+    }, [token, submit]);
+
     return (
         <>
             <header className="container">
