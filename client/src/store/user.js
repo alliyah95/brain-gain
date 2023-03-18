396a62308cfe9f347cdd09f85b4bd9ff4fc218b2
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-03-18 19:57:59 +0800

    refactor: remove token from user context

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -1,36 +1,21 @@
 import { createContext, useState } from "react";
-import User from "../../../server/models/User";
-import { getAuthToken } from "../util/auth";
 
 const UserContext = createContext({
     user: null,
-    token: null,
 });
 
 const UserContextProvider = (props) => {
     const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
 
-    const userHandler = async (id) => {
-        const currentUser = await User.findById(id);
-
-        if (currentUser) {
-            setUser(currentUser);
-        }
-    };
-
-    const tokenHandler = () => {
-        const testToken = getAuthToken();
-        setToken(testToken);
+    const userHandler = async (userData) => {
+        setUser(userData);
     };
 
     return (
         <UserContext.Provider
             value={{
                 user,
-                token,
                 onHandleUser: userHandler,
-                onHandleToken: tokenHandler,
             }}
         >
             {props.children}
