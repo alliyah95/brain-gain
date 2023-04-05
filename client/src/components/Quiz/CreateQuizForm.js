c733d94e568866d99bf1659756cf6b0f2928b317
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-04-05 19:54:34 +0800

    refactor: use single form component for creating and editing a quiz

--- client/src/components/Quiz/CreateQuizForm.js
+++ client/src/components/Quiz/CreateQuizForm.js
@@ -1,38 +1,7 @@
-import { Form, Link } from "react-router-dom";
+import QuizDescriptionForm from "./QuizDescriptionForm";
 
 const CreateQuizForm = () => {
-    return (
-        <Form
-            action=""
-            method="POST"
-            className="max-w-[1200px] mx-auto  md:p-5 xl:p-8  rounded-md mt-4 lg:mt-10 bg-opacity-80"
-        >
-            <h3 className="form-title">Create a new quiz set</h3>
-            <div className="space-y-5 mt-4 mb-10 xl:mt-10 xl:mb-16">
-                <input
-                    type="text"
-                    name="title"
-                    className="line-input"
-                    placeholder="A very easy quiz"
-                />
-                <label htmlFor="title">Title</label>
-
-                <input
-                    type="text"
-                    name="description"
-                    className="line-input"
-                    placeholder="You're in for a treat - this quiz is a breeze!"
-                />
-                <label htmlFor="description">Description</label>
-            </div>
-            <div className="flex flex-col text-center gap-y-3 lg:flex-row-reverse lg:gap-y-0 lg:gap-x-5 lg:items-center">
-                <button className="btn w-full lg:w-auto">Create set</button>
-                <Link className="link" to="/quizzes">
-                    Cancel
-                </Link>
-            </div>
-        </Form>
-    );
+    return <QuizDescriptionForm title="Create new quiz set" method="POST" />;
 };
 
 export default CreateQuizForm;
--- client/src/components/Quiz/EditQuizForm.js
+++ client/src/components/Quiz/EditQuizForm.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import ConfirmModal from "./ConfirmModal";
 import {
-    Link,
-    Form,
     useLoaderData,
     useRouteLoaderData,
     useParams,
     json,
     useNavigate,
 } from "react-router-dom";
+import QuizDescriptionForm from "./QuizDescriptionForm";
 import { toast } from "react-toastify";
 
 const EditQuizForm = () => {
@@ -55,61 +54,14 @@ const EditQuizForm = () => {
                     message="Are you sure you want to delete this quiz?"
                 />
             )}
-            <Form
-                action=""
+            <QuizDescriptionForm
+                title="Edit quiz set"
+                quizData={quizData}
                 method="PATCH"
-                className="max-w-[1200px] mx-auto md:p-5 xl:p-8  rounded-md mt-4 lg:mt-10 bg-opacity-80"
-            >
-                <h3 className="form-title flex justify-between">
-                    Edit quiz <span></span>{" "}
-                    <button
-                        className="inline-flex items-center gap-x-2 text-sm hover:text-yellow"
-                        onClick={() => {
-                            deleteModalVisibilityHandler(true);
-                        }}
-                        type="button"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            className="bi bi-trash3 w-5"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                        </svg>
-                    </button>
-                </h3>
-
-                <div className="space-y-5 mt-4 mb-10 xl:mt-10 xl:mb-16">
-                    <input
-                        type="text"
-                        name="title"
-                        className="line-input"
-                        placeholder="A very easy quiz"
-                        id="title"
-                        defaultValue={quizData.title}
-                    />
-                    <label htmlFor="title">Title</label>
-
-                    <input
-                        type="text"
-                        name="description"
-                        className="line-input"
-                        placeholder="You're in for a treat - this quiz is a breeze!"
-                        id="description"
-                        defaultValue={quizData.description}
-                    />
-                    <label>Description</label>
-                </div>
-                <div className="flex flex-col text-center gap-y-3 lg:flex-row-reverse lg:gap-y-0 lg:gap-x-5 lg:items-center">
-                    <button className="btn w-full lg:w-auto">
-                        Save Changes
-                    </button>
-                    <Link className="link" to={`/quiz/${quizData.displayId}`}>
-                        Cancel
-                    </Link>
-                </div>
-            </Form>
+                onDelete={() => {
+                    deleteModalVisibilityHandler(true);
+                }}
+            />
         </>
     );
 };
