6bc51247afb85e6bf33e4d4e68a40e6333e66352
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-04-19 20:56:01 +0800

    fix: fix error message for `404`errors

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -1,4 +1,4 @@
-import { useRouteError, useNavigate } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 import ParticlesBackground from "../components/UI/ParticlesBackground";
 import NavBar from "../components/UI/NavBar";
 import Footer from "../components/UI/Footer";
@@ -7,23 +7,18 @@ import { getAuthToken } from "../util/auth";
 const ErrorPage = ({ noNavBar }) => {
     const error = useRouteError();
     const token = getAuthToken();
-    const navigate = useNavigate();
 
     let message = "";
     if (error instanceof TypeError) {
         message =
             "There has been an internal server error. We'll try to fix it ASAP...";
-    } else if (error.status === 404 && error.data.includes("route")) {
+    } else if (error.status === 404) {
         message = "Page not found.";
     }
 
     if (error.data && error.data.message) {
         message = error.data.message;
     }
-
-    const navigationHandler = () => {
-        navigate(-1);
-    };
 
     return (
         <>
@@ -40,9 +35,9 @@ const ErrorPage = ({ noNavBar }) => {
                             An error occured!
                         </p>
                         <p className="text-base mb-4">{message}</p>
-                        <button className="btn" onClick={navigationHandler}>
-                            Go back
-                        </button>
+                        <Link className="btn" to="/home">
+                            Go Home
+                        </Link>
                     </div>
                 </main>
                 <Footer />
