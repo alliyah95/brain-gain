1d44319eb8349f897e01eed7fd36fa9543485b1c
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-04-20 16:40:11 +0800

    chore: update allowed origins

--- server/server.js
+++ server/server.js
@@ -6,8 +6,26 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins =
+    process.env.NODE_ENV === "production"
+        ? ["https://braingain.vercel.app"]
+        : ["http://localhost"];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not allow access from the specified origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+    })
+);
 
 // database config
 const URI = process.env.DB_URI;
