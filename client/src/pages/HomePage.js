0953127b3398b5b0fc6c0d90bfc69cf901ddab4e
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-05-26 00:11:15 +0800

    style: add new quiz button on home page for small screens

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import { Link, json, useLoaderData, useLocation } from "react-router-dom";
 import { getAuthToken } from "../util/auth";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import useMedia from "../hooks/useMedia";
 import QuizzesTab from "./Quiz/QuizzesTab";
 import MyActivityTab from "./Quiz/MyActivityTab";
 
@@ -8,6 +10,7 @@ const HomePage = () => {
     const data = useLoaderData();
     const queryParams = new URLSearchParams(location.search);
     const tabValue = queryParams.get("tab");
+    const { isSmallScreen } = useMedia();
 
     let tabContent = <QuizzesTab quizSets={data} />;
 
@@ -22,23 +25,39 @@ const HomePage = () => {
 
     return (
         <div className="md:px-5 xl:px-8 h-full">
-            <ul className="flex flex-row gap-4 border-b-[1px] border-b-brown py-2 mb-4 font-bold">
-                <li>
-                    <Link
+            <div className="flex flex-row items-center border-b-[1px] border-b-brown py-2 mb-4 ">
+                <ul className="font-bold flex gap-3">
+                    <li>
+                        <Link
+                            className={
+                                tabValue === "my_activity" ||
+                                tabValue === "library"
+                                    ? "opacity-50"
+                                    : ""
+                            }
+                            to="/home?tab=quizzes"
+                        >
+                            My Quiz Sets
+                        </Link>
+                    </li>
+                    <li
                         className={
-                            tabValue === "my_activity" || tabValue === "library"
-                                ? "opacity-50"
-                                : ""
+                            tabValue !== "my_activity" ? "opacity-50" : ""
                         }
-                        to="/home?tab=quizzes"
                     >
-                        My Quiz Sets
+                        <Link to="/home?tab=my_activity">My Activity</Link>
+                    </li>
+                </ul>
+                {isSmallScreen && (
+                    <Link
+                        to="/create_quiz"
+                        className="btn px-4 py-1 inline-flex items-center gap-x-2 ml-auto"
+                    >
+                        <PlusIcon className="h-3.5 w-3.5 text-light-brown fill-none stroke-current stroke-2" />
+                        New quiz
                     </Link>
-                </li>
-                <li className={tabValue !== "my_activity" ? "opacity-50" : ""}>
-                    <Link to="/home?tab=my_activity">My Activity</Link>
-                </li>
-            </ul>
+                )}
+            </div>
 
             {tabContent}
         </div>
