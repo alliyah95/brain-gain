ac265c579b409a5e795c9d00139d08a4bbbe9f9d
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-06-11 20:02:48 +0800

    feat: add paragraph on spinner

--- client/src/components/UI/Spinner.js
+++ client/src/components/UI/Spinner.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
+
 const Spinner = () => {
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowParagraph(true);
+        }, 7000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-50 z-50 flex justify-center items-center">
+        <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-50 z-50 flex flex-col justify-center items-center">
             <div className="flex">
                 <div className="relative">
-                    <div className="w-12 h-12 rounded-full absoluteborder-2 border-solid border-brown-darker border-opacity-50"></div>
-                    <div className="w-12 h-12 rounded-full animate-spin absolute border-2 border-solid border-brown-darker border-t-transparent"></div>
+                    <div className="w-12 h-12"></div>
+                    <div className="w-12 h-12 rounded-full animate-spin absolute border-4 border-solid border-brown-darker border-t-transparent"></div>
                 </div>
             </div>
+            {showParagraph && (
+                <p className="mt-24 bg-brown-darker text-sm text-white max-w-md text-center rounded-md p-4 m-4">
+                    Brain Gain is currently deployed as a free service, so
+                    loading may take a while at times. Please be patient 🥺
+                </p>
+            )}
         </div>
     );
 };
