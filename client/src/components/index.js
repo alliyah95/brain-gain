9a3ac27641dfe0b7983f2ab8157c885d7ce6bd26
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-08-14 22:34:08 +0800

    refactor: create an index file for components

---
+++ client/src/components/index.js
@@ -0,0 +1,53 @@
+import LoginForm from "./Auth/LoginForm";
+import RegistrationForm from "./Auth/RegistrationForm";
+
+import PaginationContainer from "./Pagination/PaginationContainer";
+import PaginationNav from "./Pagination/PaginationNav";
+
+import Flashcard from "./Question/Flashcard";
+import OptionInput from "./Question/OptionInput";
+import QuestionCard from "./Question/QuestionCard";
+import QuestionForm from "./Question/QuestionForm";
+import QuestionsList from "./Question/QuestionsList";
+import QuestionWithOptions from "./Question/QuestionWithOptions";
+import TrueOrFalse from "./Question/TrueOrFalse";
+
+import CreateQuizForm from "./Quiz/CreateQuizForm";
+import EditQuizForm from "./Quiz/EditQuizForm";
+import FlashcardsAndTestHeader from "./Quiz/FlashcardsAndTestHeader";
+import QuizDescriptionForm from "./Quiz/QuizDescriptionForm";
+import ShareQuiz from "./Quiz/ShareQuiz";
+
+import Card from "./UI/Card";
+import FeatureCard from "./UI/FeatureCard";
+import Footer from "./UI/Footer";
+import Modal from "./UI/Modal";
+import NavBar from "./UI/NavBar";
+import ParticlesBackground from "./UI/ParticlesBackground";
+import Spinner from "./UI/Spinner";
+
+export {
+    LoginForm,
+    RegistrationForm,
+    PaginationContainer,
+    PaginationNav,
+    Flashcard,
+    OptionInput,
+    QuestionCard,
+    QuestionForm,
+    QuestionsList,
+    QuestionWithOptions,
+    TrueOrFalse,
+    CreateQuizForm,
+    EditQuizForm,
+    FlashcardsAndTestHeader,
+    QuizDescriptionForm,
+    ShareQuiz,
+    Card,
+    FeatureCard,
+    Footer,
+    Modal,
+    NavBar,
+    ParticlesBackground,
+    Spinner,
+};
